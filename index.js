e8d4979d57b09a1fa51f7c9f2260748eb856ff65
AndreFra96 <brioschiandrea@tiscali.it>
2023-09-14 17:16:35 +0200

    express implementation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import http from "http";
+import express from "express";
 
 const users = [
   { name: "Mario", surname: "Rossi", age: 25 },
@@ -6,21 +6,20 @@ const users = [
   { name: "Silvia", surname: "Neri", age: 28 },
 ];
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/api/users") {
-    console.log(req.method);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(users));
-    res.end();
-    return;
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.write("Pagina non trovata");
-    res.end();
-    return;
-  }
+const app = express();
+
+app.use(express.json()); //Senza questo req.body è undefined
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
 });
 
-server.listen(3000, () => {
-  console.log("server is listening on port 3000");
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/api/users", (req, res) => {
+  console.log(req);
+  users.push(req.body);
+  res.json(users);
 });
